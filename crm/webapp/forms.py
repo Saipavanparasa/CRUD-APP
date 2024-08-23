ace27e970c884488c4089bc326ece69e7c32ec2d
Your Name <saipavanparasa007@gmail.com>
2024-08-23 12:11:39 +0530

    first commit

---
+++ crm/webapp/forms.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Record
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput,TextInput
+
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username']
+
+class LoginForm(AuthenticationForm):
+    username=forms.CharField(widget=TextInput())
+    password=forms.CharField(widget=PasswordInput())
+
+
+# class AddRecordForm(forms.ModelForm):
+#     class Meta:
+#         model = Record
+#         fields = ['first_name', 'last_name', 'email', 'phone','address','city','province','country']
+#create record
+class CreateRecordForm(forms.ModelForm):
+    class Meta:
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone','address','city','province','country']
+
+
+
+#update record
+class UpdateRecordForm(forms.ModelForm):
+    class Meta:
+        model=Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
+
